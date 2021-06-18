e7bf3dfc81be0181081ee7f0ecc610c9d040d3dd
sail456852 <sail456852@hotmail.com>
2021-06-18 16:01:59 +0800

    new script

---
+++ windows10/fresh-windows10-installation-script.bat
@@ -0,0 +1,10 @@
+choco install googlechrome toggl listary vim python jdk8 git maven gradle intellijidea-community cmder mobaxterm  -y
+setx PATH "%PATH%;C:\Python38" /m
+setx PATH "%PATH%;C:\ProgramData\chocolatey\lib\maven\apache-maven-3.6.2\bin" /m
+setx PATH "%PATH%;C:\Program Files\Git\cmd" /m
+setx PATH "%PATH%;C:\Program Files\Listary" /m
+setx PATH "%PATH%;C:\Program Files (x86)\Google\Chrome\Application" /m
+@pause
+
+
+
